045b533e950f87b02b9b0480ccdc74ffa9c07ce4
NatdanaiPuthom <natdanai.puthom1997@gmail.com>
2023-10-20 22:52:49 +0200

    Simple Engine V2

---
+++ Source/Engine/engine.cpp
@@ -0,0 +1,75 @@
+#include <cassert>
+#include "engine.h"
+
+Engine::Engine()
+	: myHWND(nullptr)
+	, myGraphicsEngine(nullptr)
+{
+}
+
+Engine::~Engine()
+{
+	if (myHWND != nullptr)
+		delete myHWND;
+
+	if (myGraphicsEngine != nullptr)
+		delete myGraphicsEngine;
+
+	myHWND = nullptr;
+	myGraphicsEngine = nullptr;
+}
+
+void Engine::Init(HINSTANCE& hInstance, const int aWidth, const int aHeight)
+{
+	myHWND = SetupMainWindow(hInstance, aWidth, aHeight);
+	assert(myHWND && "Failed To Create Window");
+
+	ShowWindow(*myHWND, 1);
+	UpdateWindow(*myHWND);
+
+	myGraphicsEngine = new GraphicsEngine();
+	myGraphicsEngine->Init(aHeight, aWidth, *myHWND);
+}
+
+HWND* Engine::SetupMainWindow(HINSTANCE& hInstance, const int aWidth, const int aHeight)
+{
+	WNDCLASSEXW wcex = {};
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wcex.lpfnWndProc = WndProc;
+	wcex.hInstance = hInstance;
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+	wcex.lpszClassName = L"Natdanai";
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(101));
+	//assert(wcex.hIcon != NULL && "Failed to load icon");
+
+	RegisterClassExW(&wcex);
+
+	HWND* hwnd = new HWND;
+	*hwnd = CreateWindow(
+		L"Natdanai",
+		L"SimpleEngine",
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		aWidth,
+		aHeight,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr);
+
+	return hwnd;
+}
+
+bool Engine::BeginFrame()
+{
+	return myGraphicsEngine->BeginFrame();
+}
+
+void Engine::Render()
+{
+	myGraphicsEngine->Render();
+}
+
---
+++ Source/Game/main.cpp
@@ -0,0 +1,36 @@
+#include "Engine/engine.h"
+#include "Engine/Console/Console.h"
+
+//#define WIN32_LEAN_AND_MEAN
+//#define NOMINMAX
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int /*nCmdShow*/)
+{
+	Engine engine;
+	engine.Init(hInstance, 1280, 720);
+
+	Console console;
+	console.Init();
+
+	while (engine.BeginFrame())
+	{
+		engine.Render();
+	}
+
+	return 0;
+}
+
