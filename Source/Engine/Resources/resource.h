a17aba1af26a7b67bf818faac1455efc3e4aa792
NatdanaiPuthom <natdanai.puthom1997@gmail.com>
2023-10-21 00:24:34 +0200

    Nuked. Prepare for next version

--- Source/Engine/Resources/resource.h
+++
@@ -1,6 +0,0 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Game.rc
-
-#define IDI_ICON1                       101
-#define IDI_ICON2                       102
